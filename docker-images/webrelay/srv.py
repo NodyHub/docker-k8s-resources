82109b0c57011de04a8f299cae17e23788c75a39
NodyHub <github@janharrie.de>
2020-05-31 11:35:13 +0200

    added local file lookup

--- docker-images/webrelay/srv.py
+++ docker-images/webrelay/srv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import os.path
 import requests
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib3
@@ -20,18 +21,29 @@ class MyRequestHandler(BaseHTTPRequestHandler):
     print(self.headers)
 
     print("/---------- Response -------------/")
-    dst = "%s%s" % (TARGET, self.path)
-    headers=dict(self.headers)
-    del headers['Host']
-    r = requests.get(dst, headers=headers, verify=False)
-    print(r.text,"\n\n")
+    lookup  = os.path.dirname(os.path.realpath(__file__)) + self.path
+    if os.path.isfile(lookup):
+      print("[+] Found '%s', reply with local file" % self.path)
+      with open(lookup, 'r+') as fin:
+        content = fin.readlines()
+        self.send_response(200)
+        self.send_header("Content-Length",sum(len(line) for line in content))
+        self.end_headers()
+        self.wfile.write(bytes("".join(content),"iso-8859-1"))
+    else:
+      print("[+] Forward request for '%s'" % self.path)
+      dst = "%s%s" % (TARGET, self.path)
+      headers=dict(self.headers)
+      del headers['Host']
+      r = requests.get(dst, headers=headers, verify=False)
+      print(r.text,"\n\n")
 
-    self.protocol_version=VERSION[r.raw.version]
-    self.send_response(r.status_code)
-    for h in r.headers:
-      self.send_header(h, r.headers[h])
-    self.end_headers()
-    self.wfile.write(bytes(r.text,r.headers['content-type'].split('charset=')[1]))
+      self.protocol_version=VERSION[r.raw.version]
+      self.send_response(r.status_code)
+      for h in r.headers:
+        self.send_header(h, r.headers[h])
+      self.end_headers()
+      self.wfile.write(bytes(r.text,"iso-8859-1"))
 
   def send_response(self, code, message=None):                                                                                                                                                                                              
     self.log_request(code)
