043da7d432dd4af7fc8e26d4251350ef325378a4
NodyHub <github@janharrie.de>
2020-06-16 09:29:40 +0200

    added post handler

--- docker-images/webrelay/srv.py
+++ docker-images/webrelay/srv.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import sys
+import signal
 import os
 import os.path
 import requests
@@ -14,29 +16,92 @@ VERSION = {10:"HTTP/1.0", 11:"HTTP/1.1"}
 
 class MyRequestHandler(BaseHTTPRequestHandler):
 
+  requested = []
+
   def do_GET(self):
 
-    print("/---------- Request --------------/")
-    print("GET %s %s" % (self.path, self.request_version))
-    print(self.headers)
+    seen = ("GET "+self.path in self.requested)
+    if not seen:
+      self.requested.append("GET "+self.path)
 
-    print("/---------- Response -------------/")
+
+    if not seen:
+      print("/---------- Request --------------/")
+      print("GET %s %s" % (self.path, self.request_version))
+      print(self.headers)
+
+    if not seen:
+      print("/---------- Response -------------/")
     lookup  = os.path.dirname(os.path.realpath(__file__)) + self.path
     if os.path.isfile(lookup):
-      print("[+] Found '%s', reply with local file" % self.path)
+      if not seen:
+        print("[+] Found '%s', reply with local file" % self.path)
       with open(lookup, 'r+') as fin:
         content = fin.readlines()
         self.send_response(200)
         self.send_header("Content-Length",sum(len(line) for line in content))
         self.end_headers()
         self.wfile.write(bytes("".join(content),"iso-8859-1"))
+        if not seen:
+          print("\n")
     else:
-      print("[+] Forward request for '%s'" % self.path)
+      if not seen:
+        print("[+] Forward request for '%s'" % self.path)
       dst = "%s%s" % (TARGET, self.path)
       headers=dict(self.headers)
       del headers['Host']
       r = requests.get(dst, headers=headers, verify=False)
-      print(r.text,"\n\n")
+      if not seen:
+        print(r.text,"\n")
+
+      self.protocol_version=VERSION[r.raw.version]
+      self.send_response(r.status_code)
+      for h in r.headers:
+        self.send_header(h, r.headers[h])
+      self.end_headers()
+      self.wfile.write(bytes(r.text,"iso-8859-1"))
+
+  def do_POST(self):
+
+    seen = (self.path in self.requested)
+    if not seen:
+      self.requested.append(self.path)
+
+
+    if not seen:
+      print("/---------- Request --------------/")
+      print("POST %s %s" % (self.path, self.request_version))
+      print(self.headers)
+      content_len = int(self.headers.get('Content-Length'))
+      post_body = self.rfile.read(content_len)
+      print(post_body)
+    else:
+      content_len = int(self.headers.get('Content-Length'))
+      post_body = self.rfile.read(content_len)
+
+    if not seen:
+      print("/---------- Response -------------/")
+    lookup  = os.path.dirname(os.path.realpath(__file__)) + self.path
+    if os.path.isfile(lookup):
+      if not seen:
+        print("[+] Found '%s', reply with local file" % self.path)
+      with open(lookup, 'r+') as fin:
+        content = fin.readlines()
+        self.send_response(200)
+        self.send_header("Content-Length",sum(len(line) for line in content))
+        self.end_headers()
+        self.wfile.write(bytes("".join(content),"iso-8859-1"))
+        if not seen:
+          print("\n")
+    else:
+      if not seen:
+        print("[+] Forward request for '%s'" % self.path)
+      dst = "%s%s" % (TARGET, self.path)
+      headers=dict(self.headers)
+      del headers['Host']
+      r = requests.post(dst, headers=headers, data=post_body, verify=False)
+      if not seen:
+        print(r.text,"\n")
 
       self.protocol_version=VERSION[r.raw.version]
       self.send_response(r.status_code)
@@ -50,7 +115,12 @@ class MyRequestHandler(BaseHTTPRequestHandler):
     self.send_response_only(code, message)
 
 
+def ende_im_gelaende(_signo, _stack_frame):
+  print("Got signal %s, bye bye" % _signo)
+  sys.exit(0)
 
+signal.signal(signal.SIGINT, ende_im_gelaende)
+signal.signal(signal.SIGTERM, ende_im_gelaende)
 
 Handler = MyRequestHandler
 
