a81a6d1f32dae2f38750723e3af3e50de480a8d7
NodyHub <github@janharrie.de>
2020-08-16 12:22:04 +0200

    minor bugfix. now we acces the env

--- docker-images/hello/server.js
+++ docker-images/hello/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request from ' + request.connection.remoteAddress + ' for URL: ' + request.url);
   response.writeHead(200);
-  response.end('{{HELLO_MSG}}\n');
+  response.end(process.env.HELLO_MSG+'\n');
 };
 var www = http.createServer(handleRequest);
 www.listen(80);
